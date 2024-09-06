af07353fa69dca62d483a89ef42ca83195013c91
Heather Strain <64221009+hestrain@users.noreply.github.com>
2024-09-06 15:23:13 -0400

    vite config maybe

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-  },
-});
-
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  }
+})
