59ab0ae9d9aa8f2191a024d2d5d20666612d2b71
James Parker <jamesparker.us@gmail.com>
2024-09-10 13:57:35 -0400

    GameCQ Page & CSS

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,17 +1,21 @@
 import lostSpace from "../assets/images/lostInSpace.jpg";
+
 const Error = () => {
   return (
     <div>
-      <div class="container-fluid">
-        <div class="row" style={{
+      <div className="container-fluid">
+        <div
+          className="row"
+          style={{
             backgroundImage: `url(${lostSpace})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
             height: "100vh",
-            }} >
-          <h1 class="CQfont errorTitle">Lost in the Cosmos (404)</h1>
-          <p class=" CQfont errorMessage">
+          }}
+        >
+          <h1 className="CQfont errorTitle">Lost in the Cosmos (404)</h1>
+          <p className="CQfont errorMessage">
             It seems you've ventured into uncharted territory. We couldn't find
             the page you were looking for, but don't worry, the universe is vast
             and full of mysteries.
